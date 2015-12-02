0b79eb88b309d93fe05138a1d776f04f192ce142
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2015-12-02 15:19:30 +0000

    Auto Search

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -41,6 +41,7 @@ describe('GitUserSearchController', function(){
     it('displays search results', function() {
       ctrl.searchTerm = 'hello';
       ctrl.doSearch();
+      httpBackend.flush();
       expect(ctrl.searchResult.items).toEqual(items);
     });
   });
